bd77761e48b14abab6ac8e884bd1f62f7c7b38f7
Johannes Elferich <jojotux123@hotmail.com>
2022-04-16 14:15:25 -0400

    Some musings

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,19 +16,11 @@ const toggleLocales = () => {
       <div i-carbon-campsite />
     </RouterLink>
 
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-      <div i="carbon-sun dark:carbon-moon" />
-    </button>
-
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <div i-carbon-language />
-    </a>
-
     <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
       <div i-carbon-dicom-overlay />
     </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/jojoelfe" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
