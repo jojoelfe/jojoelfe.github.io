f819a1ee10c36370d5b42c564e93dccb4211ba1e
Johannes Elferich <jojotux123@hotmail.com>
2022-04-19 20:08:48 -0400

    Nicer paper rendering

--- src/components/PaperCard.vue
+++ src/components/PaperCard.vue
@@ -6,16 +6,18 @@ console.log(props.paper)
 </script>
 
 <template lang="pug">
-div.h-64.border.rounded-xl.px-3.shadow-lg.min-w-300px.overflow-hidden.snap-center.m-3(class="not-prose" on:click="goto('https://pubmed.ncbi.nlm.nih.gov/'+props.paper.pmid)")
-  p.font-bold.leading-5.m-t-2.not-prose {{ props.paper.title }}
-  img.w-48.ml--3.float-left.m-t-auto(v-if="props.paper.image" :src="props.paper.image" alt="Paper image")
-  p.not-prose.leading-5
+div.min-h-36.border.rounded-xl.px-3.shadow-lg.min-w-30.w-36rem.max-w-36rem.overflow-hidden.m-1.not-prose
+  img.w-36.ml--3.float-left.relative.bottom-0.mr-2(v-if="props.paper.image" :src="props.paper.image" alt="Paper image")
+
+  p.font-bold.text-sm.leading-5.m-t-2.not-prose {{ props.paper.title }}
+  p.not-prose.leading-5.text-xs
     span(v-for="(author, index) in props.paper.authors")
       span(v-bind:class="{ 'font-bold': author.includes('Elferich')}") {{author}}
       span(v-if="index < props.paper.authors.length - 1") ,&nbsp;
-  p.not-prose.leading-5
+  p.not-prose.leading-5.text-xs
     span.font-bold {{props.paper.fulljournal}}
     span &nbsp;{{props.paper.date}}
-  p.not-prose
-    div.inline-block(i-academicons:pubmed)
+  a.not-prose(:href="'https://pubmed.ncbi.nlm.nih.gov/'+props.paper.pmid" target="_blank")
+    <div class="inline-block" i-academicons:pubmed></div>
+
 </template>
--- src/components/PaperList.vue
+++ src/components/PaperList.vue
@@ -1,10 +1,35 @@
 <script lang="ts" setup>
 import papers from '~/data/papers.json'
+const show_more = ref(false)
 
+const papers_filtered = computed(() => {
+  // If show_more return all otherwise only the first 4 papers
+  return show_more.value ? papers : papers.slice(0, 4)
+})
 </script>
 
 <template>
   <div class="flex flex-wrap">
-    <PaperCard v-for="(paper, index) in papers" :key="index" :paper="paper" />
+    <PaperCard
+      v-for="(paper, index) in papers_filtered"
+      :key="index" :paper="paper"
+    />
+  </div>
+  <div class="flex justify-center mt-2">
+    <button
+      v-if="!show_more"
+      class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full"
+      @click="show_more = true"
+    >
+      Show more
+    </button>
+
+    <button
+      v-if="show_more"
+      class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full"
+      @click="show_more = false"
+    >
+      Show less
+    </button>
   </div>
 </template>
